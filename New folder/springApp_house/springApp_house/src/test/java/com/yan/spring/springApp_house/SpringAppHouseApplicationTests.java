9f18680ef065c5954eadbdb26065478232c635b1
yanuvelir <yanuvelir@gmail.com>
2024-07-31 20:46:36 +0300

    commited

---
+++ New folder/springApp_house/springApp_house/src/test/java/com/yan/spring/springApp_house/SpringAppHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.spring.springApp_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
